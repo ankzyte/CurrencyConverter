0351bb3a991707377c4a6872e14ca0a86369dd17
ankzyte <ankitsinghbl2003@gmail.com>
2024-01-17 20:36:07 +0530

    add files

---
+++ script.js
@@ -0,0 +1,27 @@
+let fromCurrency = "INR";
+let toCurrency = "USD";
+let amount = document.querySelector("#amount");
+let from = document.querySelector("#from");
+let to = document.querySelector("#to");
+let output = document.querySelector("#output");
+let rate = document.querySelector("#rate");
+let convert = document.querySelector("button");
+
+let fetchCurrencyApi = async()=>{
+    let baseUrl = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromCurrency.toLowerCase()}/${toCurrency.toLowerCase()}.json`;
+    let response = await fetch(baseUrl);
+    let data = await response.json();
+    let currValue = Number(amount.value)*Object.values(data)[1];
+    output.innerHTML = `${currValue} ${toCurrency}`;
+    rate.innerHTML = `1 ${fromCurrency} = ${Object.values(data)[1]} ${toCurrency}`;
+}
+
+fetchCurrencyApi();
+
+let convertCurrency=()=>{
+    fromCurrency = from.value;
+    toCurrency=to.value;
+    fetchCurrencyApi();
+}
+
+convert.addEventListener("click",convertCurrency);
